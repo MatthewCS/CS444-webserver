9587c4044afb9cea8c56482185f53f95d3fc7a26
Matthew <34426145+MatthewCS@users.noreply.github.com>
2022-04-12 11:34:36 -0400

    Don't broadcast variables on invalid input

--- server.c
+++ server.c
@@ -540,10 +540,11 @@ void browser_handler(int browser_socket_fd) {
             // TODO: For Part 3.1, add code here to send the error message to the browser.
             broadcast(session_id, "ERROR: Invalid input!");
         }
-
-        session_to_str(session_id, response);
-        broadcast(session_id, response);
-        save_session(session_id);
+        else {
+            session_to_str(session_id, response);
+            broadcast(session_id, response);
+            save_session(session_id);
+        }
     }
 }
 
